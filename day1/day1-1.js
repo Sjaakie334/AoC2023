05b6d094fda419fd591b7b105bb6d6f9a9ebf59f
jordy <518505@student.saxion.nl>
2023-12-03 20:21:15 +0100

    added day 1 and 2

---
+++ day1/day1-1.js
@@ -0,0 +1,20 @@
+const caliArr = [
+	'1abc2',
+	'pqr3stu8vwx',
+	'a1b2c3d4e5f',
+	'treb7uchet'
+];
+
+function getcalibrationValue () {
+	const values = [];
+	caliArr.forEach(value => {
+		const copyValue = value.replace(/[^0-9]/g, '');
+		const firstDigit = copyValue.charAt(0);
+		const lastDigit = copyValue.charAt(copyValue.length - 1);
+		const combi = `${firstDigit}${lastDigit}`;
+		values.push(combi);
+	});
+	console.log(values);
+}
+
+getcalibrationValue();
