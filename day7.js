1260d99905a095e6eb9a69415c633f8a34b46424
jordy <518505@student.saxion.nl>
2023-12-08 22:31:03 +0100

    day 7 part 2

--- day7.js
+++ day7.js
@@ -11,8 +11,9 @@ function cleverSort (a, b) {
     return 0;
 }
 
-function p1(){
-    const strength = '23456789TJQKA'
+function part(partNumber){
+    const isPartTwo = partNumber === 2;
+    const strength = (isPartTwo) ? 'J23456789TQKA' : '23456789TJQKA';
     const resultArray = lines.map(line =>{
         const splitLine = line.split(' ');
         let cards = splitLine[0];
@@ -20,13 +21,32 @@ function p1(){
         cards = cards.split('').map(card => strength.indexOf(card));
         const freq = getFrequencies(cards);
 
+        let jokers;
+        if (isPartTwo) {
+            jokers = freq['0'];
+            delete freq['0'];
+        }
+
         const hand = Object.values(freq).sort(sortDesc)
+
+        if (isPartTwo && jokers) {
+            hand[0] ??= 0;
+            hand[0] += jokers;
+        }
+
         const concatArr = hand.concat(cards);
         return {sort: concatArr, bid: Number(bid)}
     })
     const arr = resultArray.sort((a, b) => cleverSort(b.sort, a.sort))
     const finalArr = arr.map((hand, index) => hand.bid * (index + 1));
-    console.log('p1: ', sum(finalArr));
+    const resultString = `Part ${partNumber}: `;
+    console.log(resultString, sum(finalArr));
 }
 
-p1();
+part(1);
+part(2);
+/**
+ * result with actual:
+ * Part 1:  251029473
+ * Part 2:  251003917
+ */
