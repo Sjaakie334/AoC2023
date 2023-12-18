d5ace65bbdfdaf1f4d9dc9d9e3a1718f6bcd6fd2
JordyGrooten <j.grooten@construsteel.com>
2023-12-18 15:34:53 +0100

    index set to show all answers

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import { d12Result } from './day12/index.js';
 import { allResults } from './results.js';
 
 const results = allResults;
-// console.log(results);
+console.log(results);
 
-console.log(d12Result);
+// console.log(d12Result);
